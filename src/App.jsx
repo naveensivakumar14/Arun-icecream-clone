d8f1b2c7a55c89d53cdac2efa8ac8125ee76b176
naveensivakumar14 <ersnaveenkumar@gmail.com>
2025-03-22 20:27:19 +0530

    Herotop updated

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
      <Navbar/>
      <Slider/>
      <Herotop/>
-    
+
     </>
   )
 }
--- src/components/Herotop.jsx
+++ src/components/Herotop.jsx
@@ -6,19 +6,27 @@ import banner from "../assets/images/profile-banner1.jpg"
 function Herotop(){
 
     
-
     return(
-        <div className="md:h-screen md:flex justify-around items-center">
-            <div className="p-6 md:w-1/2">
-            <h1 className="text-2xl md:text-left text-center text-red-500 font-bold md:py-2">Hey, Welcome</h1>
-            <p className=" md:text-left md:text-lg md:p-0 text-justify p-4 border-none rounded-sm ">
-                to Arun Icecreams, where every scoop is crafted to perfection! Indulge in our creamy, handcrafted ice creams made with the finest ingredients. Join us for a delicious journey of unique flavors and delightful toppings. Order online or visit us to experience stores for pure frozen bliss. Taste the magic, one scoop at a time !
-            </p>
+        <div className="md:h-screen flex flex-col justify-center items-center text-center p-6">
+            {/* Section Title */}
+            <h1 className="text-3xl md:text-4xl font-bold text-red-600 mb-4"> Celebrate Your Good Times </h1>
+
+            <div className="md:flex items-center w-full max-w-6xl">
+                {/* Left Section - Text Content */}
+                <div className="p-4 md:w-1/2">
+                    <h2 className="text-2xl py-1 text-red-600 font-bold md:text-left">
+                    Hey, Welcome
+                    </h2>
+                    <p className="text-justify md:text-left md:text-lg md:p-0 p-4">
+                    Welcome to Arun Icecreams, where every scoop is crafted to perfection! Indulge in our creamy, handcrafted ice creams made with the finest ingredients. Join us for a delicious journey of unique flavors and delightful toppings. Order online or visit our store to experience pure frozen bliss. Taste the magic, one scoop at a time!
+                    </p>
+                </div>
+                {/* Right Section - Image */}
+                <div className="m-4 md:w-1/2 flex justify-center">
+                    <img className="md:w-[600px] w-full rounded-lg" src={banner} alt="banner-image" />
+                </div>
             </div>
-            <div className="m-4" >
-                <img className="md:w-[700px]" src={banner} alt="" />
-            </div>
-        </div>
+        </div>  
     )
 
 }
