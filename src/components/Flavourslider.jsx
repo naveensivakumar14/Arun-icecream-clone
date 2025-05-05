2f8f95c769d8e70ac885a8dac1ecbdcaab19b928
naveensivakumar14 <ersnaveenkumar@gmail.com>
2025-05-05 23:51:48 +0530

    Arun icecreams updated

--- src/components/Flavourslider.jsx
+++ src/components/Flavourslider.jsx
@@ -7,7 +7,7 @@ const flavors = [
   { emoji: "🫐", name: "Blackcurrent" },
   { emoji: "🍫", name: "ChocoBelgion" },
   { emoji: "🍓", name: "Strawberry" },
-  { emoji: "🍒", name: "Icones" },
+  { emoji: "🍒", name: "Triplefruit" },
 ];
 
 function FlavorSlider() {
