964150bdf99864c0302f1a5f69a20674dd0f3fe2
naveensivakumar14 <ersnaveenkumar@gmail.com>
2025-07-07 14:33:43 +0530

    Website updated

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -40,14 +40,13 @@ function Testimonial() {
     <div className="max-w-xl mx-auto text-center space-y-6 py-10 px-6 md:py-20 rounded-xl transition-all duration-500">
 
      <h2 className="md:text-2xl font-bold">Our Every Scoop Has a Story</h2>
-
       {/* Decorative Quote Icon */}
       <svg xmlns="http://www.w3.org/2000/svg" className="w-8 fill-pink-500 mx-auto" viewBox="0 0 35 30" fill="none">
         <path d="M22.3838 27.6777C23.5264 28.9961 25.3721 29.6992 27.4814 29.6992C31.6123 29.6992 34.249 26.9746 34.249 22.7559C34.249 18.625 31.5244 15.6367 27.6572 15.6367C26.8662 15.6367 25.9873 15.8125 25.1084 16.0762C24.5811 9.48438 27.833 4.03516 32.2275 2.36523L31.7881 0.871094C24.2295 3.77148 19.4834 11.1543 19.4834 19.8555C19.4834 22.668 20.5381 25.7441 22.3838 27.6777ZM0.499023 19.8555C0.499023 24.6895 3.22363 29.6992 8.49707 29.6992C12.54 29.6992 15.1768 26.9746 15.1768 22.7559C15.1768 18.625 12.4521 15.6367 8.67285 15.6367C7.88184 15.6367 7.00293 15.8125 6.12402 16.0762C5.59668 9.48438 8.84863 4.03516 13.2432 2.36523L12.7158 0.871094C5.24512 3.77148 0.499023 11.1543 0.499023 19.8555Z" />
       </svg>
 
       {/* Testimonial Text */}
-      <p className="text-lg md:text-xl leading-relaxed text-gray-700">
+      <p className="text-sm md:text-xl leading-relaxed text-gray-700">
         "{current.comment}"
       </p>
 
