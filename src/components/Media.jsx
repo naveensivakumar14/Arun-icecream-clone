e1f6670eba31014d27aae0b2851183b56f43fd14
naveensivakumar14 <ersnaveenkumar@gmail.com>
2025-07-26 21:35:30 +0530

    Media page updated

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -5,7 +5,6 @@ const Media = () => {
   return (
     <div style={{ padding: '2rem' }}>
       <h1>Media Gallery</h1>
-      <p>Images, videos, and advertisements of Arun Ice Creams will appear here.</p>
     </div>
   );
 };
